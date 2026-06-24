f701e502bd5fb5530eb52ac7be2a4811128bd13f
Shubha-KA <shubhaambareesh@gmail.com>
2026-06-24 15:48:45 +0530

    fix: terraform destroy

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -19,7 +19,11 @@ provider "azurerm" {
   storage_use_azuread = true
   resource_provider_registrations = "none"
 
-  features {}
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
 }
 
 data "azurerm_client_config" "current" {}
